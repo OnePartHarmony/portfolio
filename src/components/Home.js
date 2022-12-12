01316ff356a771b5438ffadfaa50a3abe84ce0a1
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2022-12-12 17:27:53 -0500

    project section on home page, gradient header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
 import React from "react"
 import ContactForm from "./ContactForm"
 import Header from "./Header"
+import ProjectIndex from "./ProjectIndex"
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
+            <div className="homeProjects">
+                <h2>Check Out Some of My Projects:</h2>
+                <div className="sectionWrap">
+                    <section>
+                        <ProjectIndex />                    
+                    </section>                    
+                </div>
+            </div>            
             <ContactForm />
-            {/* <section>Contact Me</section> */}
+            <footer>&copy;2022 Harmony Luise Brown</footer>
         </div>
     )
 }
