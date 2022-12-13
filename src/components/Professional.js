113b81859c579b5e4cd4c0e71f11dba37c405767
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2022-12-13 12:08:34 -0500

    complete about me

--- src/components/Professional.js
+++ src/components/Professional.js
@@ -3,7 +3,7 @@ import React from "react"
 const Professional = () => {
 
     return (
-        <p>I am a big ol' nerd who experiences the world as numbers and logic.  In my years as a professioinal baker, I saw baking bread as engineering, and strived for improvements with each daily bake.  I recently returned to my roots as a mathlete and took a software engineering immersive through General Assembly, where I found I have an ability to learn quickly and a love of coding.  To me, it's been a natural shift to a new form of engineering, where I can instantly see the results of an experiment.</p>
+        <p>I am a big ol' nerd who experiences the world as numbers and logic.  In my years as a professioinal baker, I saw baking bread as engineering and strived for improvements with each daily bake.  I recently returned to my roots as a mathlete and took a software engineering immersive through General Assembly, where I found I have an ability to learn quickly and a love of coding.  To me, it's been a natural shift to a new form of engineering, where I can instantly see the results of an experiment.</p>
     )
 }
 
