7a5a614153cebb50d9253e0b18ca6b52f4fcb817
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2022-12-14 18:45:18 -0500

    photo change, font sizes

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -33,15 +33,15 @@ const ContactForm = () => {
             <h2>Contact Me {"\u2794"}<span> harmonyluisebrown@gmail.com</span></h2>
             <section className="inputs">
                 <div>
-                    <label htmlFor="user_name" className="me-3">Name:</label><br/>
+                    <label htmlFor="user_name">Name:</label><br/>
                     <input type="text" name="user_name" id="user_name" placeholder="your full name"/><br/>
-                    <label htmlFor="address" className="mt-3 me-3">Email Address:</label><br/>
+                    <label htmlFor="address" className="emailLabel">Email Address:</label><br/>
                     <input type="text" name="address" id="address" placeholder="your email address"/><br/>
                 </div>
                 <div>
-                    <label htmlFor="message" className="me-3">Message:</label><br/>
+                    <label htmlFor="message">Message:</label><br/>
                     <textarea name="message" id="message" placeholder="what would you like to tell me?"/><br/>
-                    <button type="submit" className="float-end mt-3">Send</button>    
+                    <button type="submit" className="float-end">Send</button>    
                 </div>
             </section>
         </form>
