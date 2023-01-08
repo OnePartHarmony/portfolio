40100349de3f95a5f72e48d99596dc95044c371f
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2023-01-08 17:28:44 -0500

    added transition to new links hover colour

--- src/components/ProjectShow.js
+++ src/components/ProjectShow.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { useParams } from "react-router-dom"
 import currentProjects from "../CurrentProjects"
 import { Link } from "react-router-dom"
