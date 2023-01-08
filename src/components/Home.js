bf6eee16221a9d9159754b7d51ac6e0f5b8d1259
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2023-01-08 17:05:58 -0500

    turned headers into links on home page, changes nav about me to who?, hovering over about me changes text to more about me,  all attempting to clarify that clicking will lead to more than just the about me shown on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 import ContactForm from "./ContactForm"
 import Header from "./Header"
 import Professional from "./Professional"
 import ProjectIndex from "./ProjectIndex"
+import { Link } from "react-router-dom"
 
 const Home = () => {
+
+    const [hoverAbout, setHoverAbout] = useState(false)
+
     return (
         <div className="home">
             <Header />
             <div className="homeAbout">
-                <h2>About Me</h2>
+                <Link to="/portfolio/aboutHarmony"><h2 onMouseEnter={() => {setHoverAbout(true)}} onMouseLeave={() => setHoverAbout(false)}>{hoverAbout ? "More About Me" : "About Me" }</h2></Link>
                 <Professional />
             </div>
             <div className="homeProjects">
-                <h2>Check Out Some of My Projects:</h2>
+                <Link to="/portfolio/projects"><h2>Check Out Some of My Projects:</h2></Link>
                 <div className="sectionWrap">
                     <section className="homeProjectsSection">
                         <ProjectIndex />                    
