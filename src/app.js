ac75ff8fa7b4de7a2c8a3f704285884c832f8936
OnePartHarmony <107879442+OnePartHarmony@users.noreply.github.com>
2023-01-29 14:39:52 -0500

    setup file for app to change page content in single route

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react"
+
+const App = () => {
+
+    const [currentPage, setCurrentPage] = useState("home")
+
+    return (
+        <></>
+    )
+}
+
+export default App
